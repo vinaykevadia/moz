29dbdb4f49f73e611143acfe4eb0250ce45eccc7
David Wickström <davidwickstrom@gmail.com>
2014-12-17 22:36:08 +0100

    Initial commit

---
+++ spec/SeoMoz/RequestSpec.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace spec\SeoMoz;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use SeoMoz\Client\Client;
+use SeoMoz\Generator\RequestUrl;
+use SeoMoz\Validator\ResponseValidator;
+use stdClass;
+
+class RequestSpec extends ObjectBehavior
+{
+    function let(
+        RequestUrl $requestUrlGenerator,
+        Client $client,
+        ResponseValidator $responseValidator
+    ) {
+        $this->beConstructedWith($requestUrlGenerator, $client, $responseValidator);
+    }
+
+    function it_makes_valid_requests_to_the_moz_api(
+        RequestUrl $requestUrlGenerator,
+        Client $client,
+        ResponseValidator $responseValidator
+    ) {
+        $requestUrlGenerator->generate('sunet.se')->shouldBeCalled()->willReturn('foo');
+        $dummy = '{"foo": "bar"}';
+        $client->get('foo')->shouldBeCalled()->willReturn($dummy);
+        $responseValidator->validate($dummy)->shouldBeCalled();
+        $this->make('sunet.se')->shouldBeAnInstanceOf('SeoMoz\Response');
+    }
+
+}
---
+++ src/SeoMoz/Generator/RequestUrl.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace SeoMoz\Generator;
+
+class RequestUrl
+{
+    /**
+     * @var
+     */
+    private $accessId;
+    /**
+     * @var
+     */
+    private $secretKey;
+
+    /**
+     * @param $accessId
+     * @param $secretKey
+     */
+    public function __construct($accessId, $secretKey)
+    {
+        $this->accessId = $accessId;
+        $this->secretKey = $secretKey;
+    }
+
+    /**
+     * @param $domainName
+     * @return string
+     */
+    public function generate($domainName)
+    {
+        $expires = time() + 300;
+        $urlSafeSignature = $this->buildSafeSignatureUrl($expires);
+        $cols = 34359738368 + 68719476736 + 536870912 + 32;
+
+        return $this->buildUrl($domainName, $cols, $expires, $urlSafeSignature);
+    }
+
+    /**
+     * @param $domainName
+     * @param $cols
+     * @param $expires
+     * @param $urlSafeSignature
+     * @return string
+     */
+    private function buildUrl($domainName, $cols, $expires, $urlSafeSignature)
+    {
+        $parameters = [
+            "Cols" => $cols,
+            "AccessID" => $this->accessId,
+            "Expires" => $expires,
+            "Signature" => $urlSafeSignature
+        ];
+
+        $parts = [
+            'http://lsapi.seomoz.com/linkscape/url-metrics/',
+            urlencode($domainName),
+            http_build_query($parameters)
+        ];
+
+        return implode("", $parts);
+    }
+
+    /**
+     * @param $expires
+     * @return string
+     */
+    private function buildSafeSignatureUrl($expires)
+    {
+        $stringToSign = $this->accessId . "\n" . $expires;
+        $binarySignature = hash_hmac('sha1', $stringToSign, $this->secretKey, true);
+        $urlSafeSignature = urlencode(base64_encode($binarySignature));
+
+        return $urlSafeSignature;
+    }
+}
